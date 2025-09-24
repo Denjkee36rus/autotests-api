a4aead30a49ac49f13227bccec57b11651422401
Denjkee36rus <chekalkin@Chekalkin-MacBook-Pro.local>
2025-09-24 14:45:13 +0300

    create_apiclient

---
+++ clients/api_client.py
@@ -0,0 +1,48 @@
+import json
+from importlib.metadata import files
+from urllib.request import Request
+
+from httpx import Client,URL,QueryParams,Response
+from typing import Any
+
+from httpx._types import RequestData, RequestFiles
+
+
+class APIClient:
+    def __int__(self, client: Client):
+        self.client = client
+        """
+         Базовый API клиент, принимающий объект httpx.Client.
+        :param client: экземпляр httpx.Client для выполнения HTTP-запросов
+        """
+
+    def get(self, url: URL | str, params: QueryParams | None = None)->Response:
+        return self.client.get(url, params= params)
+    """
+        Выполняет GET-запрос.
+        :param url: URL-адрес эндпоинта.
+        :param params: GET-параметры запроса (например, ?key=value).
+        :return: Объект Response с данными ответа.
+    """
+
+    def post(self,
+             url: URL | str,
+             json: Any | None = None,
+             data: RequestData | None = None,
+             files: RequestFiles| None = None
+    ) -> Response:
+        return self.client.post(url,json = json, data = data, files= files)
+    """
+    Выполняет POST-запрос.
+    :param url: URL-адрес эндпоинта.
+    :param json: Данные в формате JSON.
+    :param data: Форматированные данные формы (например, application/x-www-form-urlencoded).
+    :param files: Файлы для загрузки на сервер.
+    :return: Объект Response с данными ответа.
+    """
+
+    def patch(self, url: URL | str)-> Response:
+        return self.client.patch(url, json=json)
+
+    def delete(self, url: URL | str) -> Response:
+        return self.client.delete(url)
